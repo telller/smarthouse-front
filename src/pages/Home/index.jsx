612abbe2a4b59c37c4e649e77890d26209c8abcc
Vlad Golyachenko <vgolyachenko@deloitte.com>
2018-06-26 00:13:21 +0300

    upd skeleton

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
+import { Spin, Switch, Col, Row, Slider } from 'antd'
 import LightService from 'services/light.service.js'
 import { getLightStatus } from 'store/light/actions'
 import { connect } from 'react-redux'
-import { Spin, Switch } from 'antd'
 import PropTypes from 'prop-types'
 import React from 'react'
 import './index.styl'
@@ -12,34 +12,43 @@ class Home extends React.Component {
     dispatch: PropTypes.func
   }
   state = {
-    white: false,
-    yellow: false
+    yellow: 0,
+    white: 0
   }
   componentWillMount = () => this.props.dispatch(getLightStatus())
-  componentWillReceiveProps = n => this.setState({white: !!n.lightStatus.white, yellow: !!n.lightStatus.yellow})
-  handleWhite = async () => {
-    const body = {
-      white: !this.state.white,
-      yellow: this.state.yellow
-    }
+  componentWillReceiveProps = n => this.setState({white: n.lightStatus.white, yellow: n.lightStatus.yellow})
+  toogleLed = async () => {
+    const { white, yellow } = this.state
+    const body = { white, yellow }
     const req = await LightService.toogleLed(body)
-    req && this.setState({white: !!req.white, yellow: !!req.yellow})
-  }
-  handleYellow = async () => {
-    const body = {
-      white: this.state.white,
-      yellow: !this.state.yellow
-    }
-    const req = await LightService.toogleLed(body)
-    req && this.setState({white: !!req.white, yellow: !!req.yellow})
+    req && this.setState({white: req.white, yellow: req.yellow})
   }
   render () {
+    const { white, yellow } = this.state
     return (
       <div id='home'>
         <Spin className='home' spinning={this.props.isFetchingStatus}>
           <div style={{padding: '10px 30px'}}>
-            Whitess: <Switch onChange={this.handleWhite} checked={this.state.white} />
-            Yellow: <Switch onChange={this.handleYellow} checked={this.state.yellow} />
+            <Row>
+              <Col span={12}>
+                <Slider min={0} max={255} value={white}
+                  onChange={white => this.setState({white}, () => this.toogleLed())} />
+              </Col>
+              <Col span={4}>
+                <Switch checked={!!white}
+                  onChange={() => this.setState({white: white ? 0 : 255}, () => this.toogleLed())} />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={12}>
+                <Slider min={0} max={255} value={yellow}
+                  onChange={yellow => this.setState({yellow}, () => this.toogleLed())} />
+              </Col>
+              <Col span={4}>
+                <Switch checked={!!yellow}
+                  onChange={() => this.setState({yellow: yellow ? 0 : 255}, () => this.toogleLed())} />
+              </Col>
+            </Row>
           </div>
         </Spin>
       </div>
